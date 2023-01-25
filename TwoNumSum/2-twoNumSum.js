b57bfb228b7956c6be4b85e4f12a3c98fcd4602e
nadiakhristean <nadia.khristean@gmail.com>
2023-01-25 11:22:20 -0500

    Algo 1 - Solution 2

---
+++ TwoNumSum/2-twoNumSum.js
@@ -0,0 +1,26 @@
+/*
+Approach:
+- hash table {}
+- x + y = targetSum (x = currNum)
+- If y is in hashtable, then return [x, y]
+- If not, place y in hashtable with "true" val
+*/
+
+function twoNumberSum(array, targetSum) {
+  const hash = {};
+
+  for (let i = 0; i < array.length; i++) {
+    let x = array[i];
+    let y = targetSum - x;
+
+    if (y in hash) {
+      return [x, y];
+    } else {
+      hash[x] = true;
+    }
+  }
+  return [];
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
